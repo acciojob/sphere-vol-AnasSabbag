84ab212b63b0d1fd2890a1e7851ba73c0debf517
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-22 13:12:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
-    //Write your code here
-  
+	//Write your code here
+	let r = document.getElementById("radius").value;
+	let text;
+	document.getElementById("volume").innerHTML ="";
+	if (isNaN(r) || r < 0 ) {
+	    text = "NaN";
+	} 
+	else {
+      text = 4/3*(Math.PI)*(r**3);
+	}
+  document.getElementById("volume").value = text;
+		
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
